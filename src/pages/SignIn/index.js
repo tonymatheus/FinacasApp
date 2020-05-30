6eb83b3dd7aa489700a04c12ea1106342c256101
tonymatheus <tony.chaves14@gmail.com>
2020-05-30 01:24:40 -0400

    Adiciona Tela de Login estilizada

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
+import {
+  Background,
+  Container,
+  SubmitButton,
+  SubmitText,
+  Logo,
+  Link,
+  LinkText,
+  AreaInput,
+  Input,
+} from './styles';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View>
-      <Text>Sign In</Text>
-    </View>
+    <Background>
+      <Container>
+        <Logo source={require('../../assets/Logo.png')} />
+        <AreaInput>
+          <Input
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={email}
+            onChangeText={texto => {
+              setEmail(texto);
+            }}
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            placeholder="Senha"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={password}
+            onChangeText={texto => {
+              setPassword(texto);
+            }}
+          />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link>
+          <LinkText>Criar Uma Conta</LinkText>
+        </Link>
+      </Container>
+    </Background>
   );
 }
