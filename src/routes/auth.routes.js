d7e57d7cfcc0983b6f57734b6abaeccbb3cb4335
tonymatheus <tony.chaves14@gmail.com>
2020-05-30 01:25:24 -0400

    Altera  rota  de  Sign

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,7 +7,11 @@ const AuthStack = createStackNavigator();
 function AuthRoutes() {
   return (
     <AuthStack.Navigator>
-      <AuthStack.Screen name="SignIn" component={SignIn} />
+      <AuthStack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{headerShown: false}}
+      />
     </AuthStack.Navigator>
   );
 }
