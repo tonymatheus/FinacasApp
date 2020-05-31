0e41080cdcb971c4e03e0338614d4f13e637a7f7
tonymatheus <tony.chaves14@gmail.com>
2020-05-31 11:38:56 -0400

    cria loader e adiciona condições  de  rotas  para login ou  cadastro

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
+import {Text, View, ActivityIndicator} from 'react-native';
+import {AuthConText} from '../contexts/auth';
 import AuthRoutes from './auth.routes';
-//import AppRoutes from './app.routes';
+import AppRoutes from './app.routes';
 
 function Routes() {
-  return <AuthRoutes />;
+  const {signed, loading} = useContext(AuthConText);
+
+  if (loading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" color="#131313" />
+      </View>
+    );
+  }
+
+  return signed ? <AppRoutes /> : <AuthRoutes />;
 }
 export default Routes;
