173b3315822139a17fb9d561fb582f5f290295e9
tonymatheus <tony.chaves14@gmail.com>
2020-05-31 11:35:41 -0400

    Chama dados  de  user

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text} from 'react-native';
+import {AuthConText} from '../../contexts/auth';
 
 export default function Home() {
+  const {user} = useContext(AuthConText);
   return (
     <View>
       <Text>Home</Text>
+      <Text>{user && user.nome}</Text>
+      <Text>{user && user.email}</Text>
     </View>
   );
 }
