10446e5e72f8be0e9244a3afd019e14f4e9f069b
tonymatheus <tony.chaves14@gmail.com>
2020-05-31 18:46:49 -0400

    implementa  botão  de LogOut

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,15 @@
 import React, {useContext} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {AuthConText} from '../../contexts/auth';
 
 export default function Home() {
-  const {user} = useContext(AuthConText);
+  const {user, signOut} = useContext(AuthConText);
   return (
     <View>
       <Text>Home</Text>
       <Text>{user && user.nome}</Text>
       <Text>{user && user.email}</Text>
+      <Button title="Sair" onPress={() => signOut()} />
     </View>
   );
 }
