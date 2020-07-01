72ff39856c7eec816a66fd1f422df46bd71ca7d5
tonymatheus <tony.chaves14@gmail.com>
2020-06-30 22:55:22 -0300

    Adiciona rotas de cadastro de usuário e  de perfil

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import New from '../pages/New';
 
-const AppStack = createStackNavigator();
+const AppDrawer = createDrawerNavigator();
 
 function AppRoutes() {
   return (
-    <AppStack.Navigator>
-      <AppStack.Screen name="Home " component={Home} />
-    </AppStack.Navigator>
+    <AppDrawer.Navigator
+      drawerStyle={{
+        backgroundColor: '#171717',
+      }}
+      drawerContentOptions={{
+        labelStyle: {
+          fontWeight: 'bold',
+        },
+        activeTintColor: '#fff',
+        activeBackgroundColor: '#00b94a',
+        inactiveBackgroundColor: '#000',
+        inactiveTintColor: '#DDD',
+        itemStyle: {
+          marginVertical: 5,
+        },
+      }}>
+      <AppDrawer.Screen name="Home " component={Home} />
+      <AppDrawer.Screen name="Registrar" component={New} />
+      <AppDrawer.Screen name="Profile " component={Profile} />
+    </AppDrawer.Navigator>
   );
 }
 export default AppRoutes;
