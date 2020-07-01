0f869beae324ee9bb866aaedf20616a432ad9ef1
tonymatheus <tony.chaves14@gmail.com>
2020-07-01 01:54:53 -0300

    Modifica e estiliza tela de registro de gastos

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import Header from '../../components/Header';
+import {Background, Input, SubmitButtom, SubmitText} from './styles';
 
 export default function New() {
+  const [valor, setValor] = useState('');
+  const [tipo, setTipo] = useState(null);
   return (
-    <View>
-      <Text>New</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Background>
+        <Header />
+        <SafeAreaView style={{alignItems: 'center'}}>
+          <Input
+            placeholder="Valor Desejado"
+            keyboardType="numeric"
+            returnKeyType="next"
+            onSubmitEditing={() => Keyboard.dismiss()}
+            value={valor}
+            onChangeText={text => setValor(text)}
+          />
+
+          <SubmitButtom>
+            <SubmitText>Resgistrar</SubmitText>
+          </SubmitButtom>
+        </SafeAreaView>
+      </Background>
+    </TouchableWithoutFeedback>
   );
 }
