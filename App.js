1938f3e97232d27f29e04d70d38eb75e683e6f4d
tonymatheus <tony.chaves14@gmail.com>
2020-07-17 22:02:21 -0300

    Remove import não utiliado

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {View, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
 import Routes from './src/routes';
 import AuthProvider from './src/contexts/auth';
 
