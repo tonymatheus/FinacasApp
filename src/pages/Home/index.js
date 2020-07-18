317cb2b12c220fb62df74f75601d7fbd680bb85b
tonymatheus <tony.chaves14@gmail.com>
2020-07-18 07:40:35 -0300

    Remove importe não utilizado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState, Children} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 
 import firebase from '../../Services/FirebaseConnection';
 import {AuthConText} from '../../contexts/auth';
