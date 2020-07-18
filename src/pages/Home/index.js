c6979954bf9c46af739bdc7c28dacf0a403bc710
tonymatheus <tony.chaves14@gmail.com>
2020-07-18 07:37:14 -0300

    Apresenta de saldo e histórico vindos atualizados do banco de dados

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,61 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState, Children} from 'react';
 
+import firebase from '../../Services/FirebaseConnection';
 import {AuthConText} from '../../contexts/auth';
 import Header from '../../components/Header';
 import HistoricoList from '../../components/HistoricoList';
 import {Background, Container, List, Nome, Saldo, Title} from './styles';
+import {format} from 'date-fns';
 
 export default function Home() {
   const {user} = useContext(AuthConText);
-  const [historico, setHistorico] = useState([
-    {key: '1', tipo: 'receita', valor: 112000},
-    {key: '2', tipo: 'despesa', valor: 500},
-    {key: '3', tipo: 'receita', valor: 50},
-    {key: '4', tipo: 'despesa', valor: 100},
-    {key: '5', tipo: 'receita', valor: 3200},
-    {key: '6', tipo: 'receita', valor: 5000},
-    {key: '7', tipo: 'receita', valor: 8000},
-    {key: '8', tipo: 'despesa', valor: 9000},
-    {key: '9', tipo: 'despesa', valor: 6000},
-    {key: '10', tipo: 'receita', valor: 2000},
-    {key: '11', tipo: 'receita', valor: 200.0},
-  ]);
+  const [historico, setHistorico] = useState([]);
+
+  const [saldo, setSaldo] = useState(0);
+  const uid = user && user.uid;
+
+  useEffect(() => {
+    async function loadList() {
+      await firebase
+        .database()
+        .ref('users')
+        .child(uid)
+        .on('value', snapshot => {
+          setSaldo(snapshot.val().saldo);
+        });
+
+      await firebase
+        .database()
+        .ref('historico')
+        .child(uid)
+        .orderByChild('date')
+        .equalTo(format(new Date(), 'dd/MM/yy'))
+        .limitToLast(10)
+        .on('value', snapshot => {
+          setHistorico([]);
+
+          snapshot.forEach(children => {
+            let list = {
+              key: children.key,
+              tipo: children.val().tipo,
+              valor: children.val().valor,
+            };
+
+            setHistorico(oldArray => [...oldArray, list].reverse());
+          });
+        });
+    }
+    loadList();
+  }, []);
+
   return (
     <Background>
       <Header />
       <Container>
         <Nome>{user && user.nome}</Nome>
-        <Saldo>R$10.0000</Saldo>
+        <Saldo>
+          R$ {saldo.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}
+        </Saldo>
       </Container>
 
       <Title>Ultimas Movimentações</Title>
