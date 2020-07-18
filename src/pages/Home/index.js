56a7bbdc1a4c4bef60ca55c1d841d0ca19e654fa
tonymatheus <tony.chaves14@gmail.com>
2020-07-18 11:02:26 -0300

    Cria função para deletar registros ao clicar e cria condição para calcular adiferença no resultado final do saldo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,25 @@
 import React, {useContext, useEffect, useState} from 'react';
-
+import {Alert} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import firebase from '../../Services/FirebaseConnection';
 import {AuthConText} from '../../contexts/auth';
 import Header from '../../components/Header';
 import HistoricoList from '../../components/HistoricoList';
-import {Background, Container, List, Nome, Saldo, Title} from './styles';
-import {format} from 'date-fns';
+import {
+  Background,
+  Container,
+  List,
+  Nome,
+  Saldo,
+  Title,
+  BtnRegistrar,
+} from './styles';
+import {format, isPast} from 'date-fns';
 
 export default function Home() {
   const {user} = useContext(AuthConText);
   const [historico, setHistorico] = useState([]);
-
+  const navigation = useNavigation();
   const [saldo, setSaldo] = useState(0);
   const uid = user && user.uid;
 
@@ -34,11 +43,12 @@ export default function Home() {
         .on('value', snapshot => {
           setHistorico([]);
 
-          snapshot.forEach(children => {
+          snapshot.forEach(childItem => {
             let list = {
-              key: children.key,
-              tipo: children.val().tipo,
-              valor: children.val().valor,
+              key: childItem.key,
+              tipo: childItem.val().tipo,
+              valor: childItem.val().valor,
+              date: childItem.val().date,
             };
 
             setHistorico(oldArray => [...oldArray, list].reverse());
@@ -48,6 +58,53 @@ export default function Home() {
     loadList();
   }, []);
 
+  function handleDelete(data) {
+    if (isPast(new Date(data.date))) {
+      //se  a data já tiver passado a condição entra aqui
+      alert('Você não pode excluir um resgistro antigo!');
+      return;
+    }
+    Alert.alert(
+      'Cuidado Atenção!',
+      `Você deseja excluir ${data.tipo} - Valor: ${data.valor}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'COntinuar',
+          onPress: () => handleDeleteSuccess(data),
+        },
+      ],
+    );
+  }
+
+  async function handleDeleteSuccess(data) {
+    await firebase
+      .database()
+      .ref('historico')
+      .child(uid)
+      .child(data.key)
+      .remove()
+      .then(async () => {
+        let saldoAtual = saldo;
+        data.tipo === 'despesa'
+          ? (saldoAtual += parseFloat(data.valor))
+          : (saldoAtual -= parseFloat(data.valor));
+
+        await firebase
+          .database()
+          .ref('users')
+          .child(uid)
+          .child('saldo')
+          .set(saldoAtual);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
     <Background>
       <Header />
@@ -56,6 +113,9 @@ export default function Home() {
         <Saldo>
           R$ {saldo.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}
         </Saldo>
+        <BtnRegistrar onPress={() => navigation.navigate('Registrar')}>
+          <Title>Resgistrar</Title>
+        </BtnRegistrar>
       </Container>
 
       <Title>Ultimas Movimentações</Title>
@@ -64,7 +124,9 @@ export default function Home() {
         showsVerticalScrollIndicator={false}
         data={historico}
         keyExtractor={item => item.key}
-        renderItem={({item}) => <HistoricoList data={item} />}
+        renderItem={({item}) => (
+          <HistoricoList data={item} deleteItem={handleDelete} />
+        )}
       />
     </Background>
   );
