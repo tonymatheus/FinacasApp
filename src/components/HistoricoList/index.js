ee1130fa1253f87765c2ba9cb5dacba11b51a440
tonymatheus <tony.chaves14@gmail.com>
2020-07-18 10:54:56 -0300

    Rece por parâmetro função que deleta dados

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import {TouchableWithoutFeedback} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import {Container, Tipo, IconView, TipoText, ValorText} from './styles';
-export default function HistoricoList({data}) {
+
+export default function HistoricoList({data, deleteItem}) {
   return (
-    <Container>
-      <Tipo>
-        <IconView tipo={data.tipo}>
-          <Icon
-            name={data.tipo === 'despesa' ? 'arrow-down' : 'arrow-up'}
-            color="#fff"
-            size={20}
-          />
-          <TipoText>{data.tipo}</TipoText>
-        </IconView>
-      </Tipo>
-      <ValorText>R${data.valor}</ValorText>
-    </Container>
+    <TouchableWithoutFeedback onLongPress={() => deleteItem(data)}>
+      <Container>
+        <Tipo>
+          <IconView tipo={data.tipo}>
+            <Icon
+              name={data.tipo === 'despesa' ? 'arrow-down' : 'arrow-up'}
+              color="#fff"
+              size={20}
+            />
+            <TipoText>{data.tipo}</TipoText>
+          </IconView>
+        </Tipo>
+        <ValorText>R${data.valor}</ValorText>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
