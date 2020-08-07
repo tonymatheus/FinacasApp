939528cc94756dbd1d57305478e323ad97cefbed
tonymatheus <tony.chaves14@gmail.com>
2020-08-07 00:24:22 -0300

    Adiciona Drawer content passando o componente de  drawer personalizado

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -3,12 +3,14 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
 import New from '../pages/New';
+import CustomDrawer from '../components/CustomDrawer';
 
 const AppDrawer = createDrawerNavigator();
 
 function AppRoutes() {
   return (
     <AppDrawer.Navigator
+      drawerContent={props => <CustomDrawer {...props} />}
       drawerStyle={{
         backgroundColor: '#171717',
       }}
